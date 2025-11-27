c1442bf3a55f199dabbd915e5f8f02e47c2c2e0c
Nicholas Barnett <133896112+thenicholasbarnett@users.noreply.github.com>
2025-11-27 11:21:54 +0100

    Add files via upload

--- executable_files/condor/run_py_template.sh
+++ executable_files/condor/run_py_template.sh
@@ -1,4 +1,7 @@
 #!/bin/sh --login
 #BSUB -q 1nh
-cmsenv
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd /afs/cern.ch/user/n/nbarnett/public/CondorWorkArea/CMSSW_15_1_0_patch3/src
+eval `scramv1 runtime -sh`
+exec > /dev/null 2>&1
 cmsRun __EXECUTABLE_FILE__ $1 $2
